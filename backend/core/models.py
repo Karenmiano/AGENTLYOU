414fb410a2f1cc7626a74bf5cf58aeeb95855b04
Karen Miano <mianokaren1798@gmail.com>
2025-07-02 18:25:15 +0300

    Changed location.state_province to location.state_region

--- backend/core/models.py
+++ backend/core/models.py
@@ -7,13 +7,13 @@ class Location(models.Model):
     """
 
     city = models.CharField(max_length=255)
-    state_province = models.CharField(max_length=255, blank=True, default="")
+    state_region = models.CharField(max_length=255, blank=True, default="")
     country = models.CharField(max_length=255)
 
     # add a unique constraint to city, state_province, country combo
     class Meta:
         db_table = "locations"
-        unique_together = ("city", "state_province", "country")
+        unique_together = ("city", "state_region", "country")
 
     def __str__(self):
         return f"{self.city}, {self.state_province}, {self.country}"
