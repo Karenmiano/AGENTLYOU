3111057e026f511e77b6c8f84f8dab6760fe6eb6
Karen Miano <mianokaren1798@gmail.com>
2025-10-17 15:52:03 +0300

    feat(backend): Add end point for clients to retrieve a gig

--- backend/gigs/urls.py
+++ backend/gigs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from gigs.views import GigCreateView, GigUpdateView, PublishGig
+from gigs.views import GigCreateView, GigClientReviewView, GigUpdateView, PublishGig
 
 urlpatterns = [
     path("new/", GigCreateView.as_view(), name="gig-create"),
+    path("<uuid:pk>/review/", GigClientReviewView.as_view(), name="gig-client-review"),
     path("<uuid:pk>/edit/", GigUpdateView.as_view(), name="gig-update"),
     path("<uuid:pk>/publish/", PublishGig.as_view(), name="gig-publish"),
 ]
